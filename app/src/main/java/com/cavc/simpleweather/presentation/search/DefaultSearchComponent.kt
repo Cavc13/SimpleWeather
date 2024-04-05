859d97403fc89439cb5d479b97347b59e953f476
Konstantin <13liver@mail.ru>
2024-04-05 17:02:29 +0300

    Assisted Inject

--- app/src/main/java/com/cavc/simpleweather/presentation/search/DefaultSearchComponent.kt
+++ app/src/main/java/com/cavc/simpleweather/presentation/search/DefaultSearchComponent.kt
@@ -6,18 +6,20 @@ import com.arkivanov.mvikotlin.extensions.coroutines.labels
 import com.arkivanov.mvikotlin.extensions.coroutines.stateFlow
 import com.cavc.simpleweather.domain.entity.City
 import com.cavc.simpleweather.presentation.extensions.componentScope
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-class DefaultSearchComponent @Inject constructor(
-    private val reasonOpening: ReasonOpening,
+class DefaultSearchComponent @AssistedInject constructor(
     private val searchStoreFactory: SearchStoreFactory,
-    private val onBackClicked: () -> Unit,
-    private val onCitySavedToFavourite: () -> Unit,
-    private val onForecastForCityRequested: (City) -> Unit,
-    componentContext: ComponentContext
+    @Assisted("reasonOpening") private val reasonOpening: ReasonOpening,
+    @Assisted("onBackClicked") private val onBackClicked: () -> Unit,
+    @Assisted("onCitySavedToFavourite") private val onCitySavedToFavourite: () -> Unit,
+    @Assisted("onForecastForCityRequested") private val onForecastForCityRequested: (City) -> Unit,
+    @Assisted("componentContext") componentContext: ComponentContext
 ) : SearchComponent, ComponentContext by componentContext {
 
     private val store = instanceKeeper.getStore {
@@ -63,5 +65,16 @@ class DefaultSearchComponent @Inject constructor(
 
     override fun onClickCity(city: City) {
         store.accept(SearchStore.Intent.ClickCity(city))
+    }
+
+    @AssistedFactory
+    interface Factory {
+        fun create(
+            @Assisted("reasonOpening") reasonOpening: ReasonOpening,
+            @Assisted("onBackClicked") onBackClicked: () -> Unit,
+            @Assisted("onCitySavedToFavourite") onCitySavedToFavourite: () -> Unit,
+            @Assisted("onForecastForCityRequested") onForecastForCityRequested: (City) -> Unit,
+            @Assisted("componentContext") componentContext: ComponentContext
+        ) : DefaultSearchComponent
     }
 }
